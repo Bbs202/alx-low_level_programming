b56c19a0865aec72cd7be0d3ab0476ef35a983e0
Bbs202 <bolajibidemi132@gmail.com>
2021-06-17 08:12:00 -0700

    Changes to file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<string.h>
+/* Main function prints string*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts(""Programming is like building a multilingual puzzle\n");
 return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+/*main function prints string*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
