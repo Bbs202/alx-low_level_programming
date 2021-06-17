86ad32d4fd555c07571772a1ebac20b4fc36d454
Bbs202 <bolajibidemi132@gmail.com>
2021-06-17 11:40:59 -0700

    Changes to file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
 * main - prints string
 *
-*Return - 1
+* Return - 1
 */
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+strcpy("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ long long int unsignedLongIntType;
 char charType;
 printf("size of an int: %zu byte(s)\n", sizeof(intType));
 printf("size of a float: %zu byte(s)\n", sizeof(floatType));
-printf("size of a long: %zu byte(s)\n", sizeof(longType));
+printf("size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("size of a long long int: %zu byte(s)\n", sizeof(unsignedLongIntType));
-printf("size of a char(s): %zu byte(s)\n", sizeof(charType));
+printf("size of a char: %zu byte(s)\n", sizeof(charType));
 
 return (0);
 }
