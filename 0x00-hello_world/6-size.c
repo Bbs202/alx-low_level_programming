cfc565130b87d9c41cd325a2d94d1e6c24fcd424
Bbs202 <bolajibidemi132@gmail.com>
2021-06-17 13:14:00 -0700

    Changes to file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ float floatType;
 long int longType;
 long long int unsignedLongIntType;
 char charType;
-printf("size of char: %zu byte(s)\n", sizeof(charType));
-printf("size of an int: %zu byte(s)\n", sizeof(intType));
-printf("size of a long int: %zu byte(s)\n", sizeof(longType));
-printf("size of a long long int: %zu byte(s)\n", sizeof(unsignedLongIntType));
-printf("size of a float: %zu byte(S)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(unsignedLongIntType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
