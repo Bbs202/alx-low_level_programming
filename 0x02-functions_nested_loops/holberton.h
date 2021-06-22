79cb9738c0074f3d6826b1673551bf44e192f12b
Bbs202 <bolajibidemi132@gmail.com>
2021-06-22 10:17:29 -0700

    Added file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,4 +1,8 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
+int _islower(int);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int);
+int _isalpha(int c);
