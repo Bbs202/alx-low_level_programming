672fbe1a9ca152f08483d7c1d9ac588db2ca8038
Bbs202 <bolajibidemi132@gmail.com>
2021-06-23 05:05:56 -0700

    Added new files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* positive_or_negative - prints zero if n is zero
+*
+* @n: integer
+*
+* Return: 0
+*/
+int positive_or_negative(int n)
+{
+if (n == 0)
+{
+printf("%d is zero", n);
+}
+return (0);
+}
