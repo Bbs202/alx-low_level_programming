392843ee9cfb71871ded89556de40ee56fd6d0bd
Bbs202 <bolajibidemi132@gmail.com>
2021-06-23 07:10:17 -0700

    Added new files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
 #include "holberton.h"
+
 /**
 * main - prints zero if n is zero
 *
 * Return: 0
 */
+
 int main(void)
 {
-int a;
-a = 0;
-positive_or_negative(a);
+int i;
+
+i = 0;
+positive_or_negative(i);
+
 return (0);
 }
