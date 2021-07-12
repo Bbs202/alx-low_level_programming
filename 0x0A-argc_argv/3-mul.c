7dee5853ae27b9332f9c3571c897ea552485df69
Bbs202 <bolajibidemi132@gmail.com>
2021-07-12 13:07:46 -0700

    Changes to file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 /**
  * main - Print the name of the program
@@ -21,25 +21,21 @@ int main(int argc, char *argv[])
 	(void) argc; /*Ignore argc*/
 
 	count = 1;
-	if (argv[count] != '\0') /*Goes through the whole array*/
-	{
-		/*Condition If the program does not receive two arguments*/
-		if (argv[count] == NULL || argv[count + 1] == NULL)
+	/*Condition If the program does not receive two arguments*/
+	if (argv[count] == NULL || argv[count + 1] == NULL)
 		{
 			printf("Error\n");
 			return (1);
 		}
 
-		else /*Condition If the program does receive two arguments*/
+       	        else /*Condition If the program does receive two arguments*/
 		{
-			first_num = atoi(argv[count]); /*ATOI --> convert string to int*/
+			first_num = atoi(argv[count]);
 			second_num = atoi(argv[count + 1]);
 			result = first_num * second_num;
 
 			printf("%d\n", result);
 		}
-
-	}
 
 	return (0);
 }
